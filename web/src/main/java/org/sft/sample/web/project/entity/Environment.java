bb04e6da2f30528a8ce7e784e001c592fba565d7
sunft <benxincaomu@foxmail.com>
2018-10-22 00:06:17 +0800

    创建项目配置的相关实体类

---
+++ web/src/main/java/org/sft/sample/web/project/entity/Environment.java
@@ -0,0 +1,39 @@
+package org.sft.sample.web.project.entity;
+
+/**
+ *  项目环境
+ * 
+ * @author sunft
+ *
+ */
+public class Environment {
+	private String id;
+	private String name;
+	private String projectId;
+	private String description;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getProjectId() {
+		return projectId;
+	}
+	public void setProjectId(String projectId) {
+		this.projectId = projectId;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+}
